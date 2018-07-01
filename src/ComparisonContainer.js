f5330cf949b3c99e5cac982806a953977a331fb7
Tory Dannenberg <rfdberg@gmail.com>
2018-07-01 16:44:45 -0600

    Fix all remaining eslint errors except line length and jest is not defined in tests

--- src/ComparisonContainer.js
+++ src/ComparisonContainer.js
@@ -6,58 +6,61 @@ import PropTypes from 'prop-types';
 const districts = new DistrictRepository();
 
 const ComparisonContainer = ( { cardArray } ) => {
-  if(cardArray.length === 0) {
-    return (<div></div>)
+  if (cardArray.length === 0) {
+    return (<div></div>);
   }
-  if(cardArray.length > 0) {
-    var newCards = cardArray.map((location, index) => {
+  if (cardArray.length > 0) {
+    var newCards = cardArray.map((location) => {
       const cardData =
         Object.keys(location).map((district, index) => { 
-          var districtAvg = districts.findAverage(district)
+          var districtAvg = districts.findAverage(district);
           const dataArray = Object.keys(location[district]).map(year => {
-            const toggle = location[district][year] <= .5 ? 'low' : 'high'
-            return (<aside key={Math.floor(Math.random() * 100000)}
-                           className={toggle}> {year}: {location[district][year]} 
-                    </aside>)
-          })
-          return (<div  className="comp-card" 
-                        key={index}>
-                  <h3>{district}</h3>
-                  <ul>{dataArray}</ul>
-                  <h4>Average: {districtAvg}</h4>
-                  </div>)
-        })
-        return cardData
-    })
+            const toggle = location[district][year] <= .5 ? 'low' : 'high';
+            return (
+              <aside key={Math.floor(Math.random() * 100000)}
+                className={toggle}> {year}: {location[district][year]} 
+              </aside>);
+          });
+          return (
+            <div  className="comp-card" 
+              key={index}>
+              <h3>{district}</h3>
+              <ul>{dataArray}</ul>
+              <h4>Average: {districtAvg}</h4>
+            </div>);
+        });
+      return cardData;
+    });
 
-    if(cardArray.length === 1) {
-      return (<div className="comp-container"> {newCards} </div>)
+    if (cardArray.length === 1) {
+      return (<div className="comp-container"> {newCards} </div>);
     } 
-    if(cardArray.length === 2) {
+    if (cardArray.length === 2) {
       const displayAvgs = cardArray.reduce((accu, district) => {
-        const title = Object.keys(district)
-        return accu.concat(title)
-      }, [])
-      const finalResult = districts.compareDistrictAverages(displayAvgs[0], displayAvgs[1])
-      const displayResult = Object.values(finalResult)
-      const displayInfo = Object.keys(finalResult)
-      return (<div className="comp-container">
-                <div> {newCards[0]}</div>
-                <div className="total-card">
-                  <h5>{displayInfo[0]}: {displayResult[0]}</h5>
-                  <h5>{displayInfo[1]}: {displayResult[1]}</h5>
-                  <h4>{displayInfo[2]}: {displayResult[2]}</h4>
-                </div>
-                <div> {newCards[1]}</div>
-              </div>
-        )
+        const title = Object.keys(district);
+        return accu.concat(title);
+      }, []);
+      const finalResult = districts.compareDistrictAverages(displayAvgs[0], displayAvgs[1]);
+      const displayResult = Object.values(finalResult);
+      const displayInfo = Object.keys(finalResult);
+      return (
+        <div className="comp-container">
+          <div> {newCards[0]}</div>
+          <div className="total-card">
+            <h5>{displayInfo[0]}: {displayResult[0]}</h5>
+            <h5>{displayInfo[1]}: {displayResult[1]}</h5>
+            <h4>{displayInfo[2]}: {displayResult[2]}</h4>
+          </div>
+          <div> {newCards[1]}</div>
+        </div>
+      );
     
     }
   }
-}
+};
 
 ComparisonContainer.propTypes = {
   cardArray: PropTypes.array
-}
+};
 
 export default ComparisonContainer;
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -3,32 +3,32 @@ import PropTypes from 'prop-types';
 
 export default class SearchForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       district: ''
-    }
+    };
   }
 
   handleInput = (event) => {
     this.setState({
       district: event.target.value
-    })
-    this.props.updateDistricts(event.target.value)
+    });
+    this.props.updateDistricts(event.target.value);
     
   }
 
   render() {
-    return(
-        <form>
-          <input 
-            type="text" 
-            placeholder="Search for your district by name"
-            aria-label="search field"
-            value={this.state.district}
-            onChange={this.handleInput}/>
-        </form>
-    )
+    return (
+      <form>
+        <input 
+          type="text" 
+          placeholder="Search for your district by name"
+          aria-label="search field"
+          value={this.state.district}
+          onChange={this.handleInput}/>
+      </form>
+    );
   }
 }
 

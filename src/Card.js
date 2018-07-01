be443b5666aeec2cb695140d97d939e4aaf54a63
Tory Dannenberg <rfdberg@gmail.com>
2018-07-01 16:25:20 -0600

    Fix eslint errors in Card and Apptest files with the exception of line length errors in test file

--- src/Card.js
+++ src/Card.js
@@ -6,24 +6,26 @@ const Card = ({ title, content, updateClickedCard }) => {
   const cardData =
 
     Object.keys(content).map((year, index) => {
-      const toggle = content[year] <= .5 ? 'low' : 'high'
-      return <aside key={index} className={toggle}> {year}: {content[year]} </aside>
+      const toggle = content[year] <= .5 ? 'low' : 'high';
+      return <aside 
+        key={index} 
+        className={toggle}> {year}: {content[year]} </aside>;
 
-    })
+    });
 
-  return(
+  return (
     <div  className="Card" 
-          onClick={() => updateClickedCard(title, content)}>
+      onClick={() => updateClickedCard(title, content)}>
       <h3>{title}</h3>
       <ul>{cardData}</ul>
     </div>
-    )
-}
+  );
+};
 
 Card.propTypes = {
   title: PropTypes.string,
   content: PropTypes.object,
   updateClickedCard: PropTypes.func
-}
+};
 
 export default Card;
